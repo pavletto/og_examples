bd6d34a6534610370da330b5068a06782279ced5
Pavel Sukhodolskii <pavel.sukhodolski@microavia.com>
2023-07-03 10:48:38 +0400

    update dependencies and move out routes

--- App.jsx
+++ App.jsx
@@ -1,26 +1,7 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import "./App.css"
+import routes from "./routes.js";
 
-const pages = import.meta.glob("./pages/**/*.jsx", {eager: true});
-
-const routes = [];
-for (const path of Object.keys(pages)) {
-    const fileName = path.match(/\.\/pages\/(.*)\.jsx$/)?.[1];
-    if (!fileName) {
-        continue;
-    }
-    const normalizedPathName = fileName.includes("$")
-        ? fileName.replace("$", ":")
-        : fileName.replace(/\/index/, "");
-
-    routes.push({
-        path: fileName === "index" ? "/" : `/${normalizedPathName.toLowerCase()}`,
-        Element: pages[path].default,
-        loader: pages[path]?.loader,
-        action: pages[path]?.action,
-        ErrorBoundary: pages[path]?.ErrorBoundary,
-    });
-}
 const router = createBrowserRouter(
     routes.map(({Element, ErrorBoundary, ...rest}) => {
         return {
@@ -40,4 +21,4 @@ function App() {
     )
 }
 
-export {routes, App}
+export {App}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import {routes} from "/App.jsx";
+import routes from "../routes.js";
 
 export default function GeoObjectExample() {
     return routes.map((route) =><a href={route.path}>{route.path}</a>)
